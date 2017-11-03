d7bae8327d0625ceedda5f675fe9e3698e803ccf
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2017-11-03 12:56:45 -0700

    Add gh-pages config

--- electric.config.js
+++ electric.config.js
@@ -3,6 +3,14 @@
 var marble = require('marble');
 
 module.exports = {
+	envOptions: {
+		ghpages: {
+			base: '/electric-test/',
+			deployOptions: {
+				branch: 'gh-pages'
+			}
+		}
+	},
 	metalComponents: ['electric-marble-components'],
 	sassOptions: {
 		includePaths: ['node_modules', marble.src]
